2db44ad3f0b480541da4b74833a35aaa4da44946
gapetrosian <gapetrosian@edu.hse.ru>
2024-12-04 19:23:53 +0300

    Добавлена операция деления (div)

--- calc.cpp
+++ calc.cpp
@@ -3,5 +3,8 @@
 extern "C" int add(int a, int b) {
     return a + b;
 }
+extern "C" int sub(int a, int b) { return a - b; }
+extern "C" int mul(int a, int b) { return a * b; }
+extern "C" int div(int a, int b) { return a / b; }
 
 
