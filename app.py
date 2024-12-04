39da5b49d74a9e286e0440c6992c6f2b12825b59
gapetrosian <gapetrosian@edu.hse.ru>
2024-12-04 19:12:08 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import ctypes
+
+calc_lib = ctypes.CDLL('./calc.dll')
+
+operations = {
+    "add": calc_lib.add,
+}
+
+class CalcInput(BaseModel):
+    type: str
+    a: int
+    b: int
+
+app = FastAPI()
+
+@app.post("/calc")
+async def calculate(input: CalcInput):
+    if input.type not in operations:
+        return {"error": "Invalid operation"}
+    operation = operations[input.type]
+    result = operation(input.a, input.b)
+    return {"result": result}
---
+++ calc.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+extern "C" int add(int a, int b) {
+    return a + b;
+}
+
+
